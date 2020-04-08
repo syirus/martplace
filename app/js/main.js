14c08cb1de49aedc4a2f8f3ee43bd6ea0cfc7c46
syirus <syirus99@gmail.com>
2020-04-08 15:47:57 +0300

    added weekly products section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function () {
 
-
+    $('.weekly-products__slider').slick({
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/content/slider/prev.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/content/slider/next.png" alt=""></button>',
+    });
 
 
 });
